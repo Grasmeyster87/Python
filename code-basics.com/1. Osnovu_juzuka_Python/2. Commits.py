6f7514b84baa1c9ac6ed4f6b44de4514a292e45b
Grameyster87 <grasmeyster@gmail.com>
2024-11-29 16:49:23 +0200

    2. Ariphmetika_v_Python

---
+++ code-basics.com/1. Osnovu_juzuka_Python/2. Commits.py
@@ -0,0 +1,8 @@
+# Удалить строку ниже после реализации задачи по регистрации
+print(10)
+
+# For Winterfell!
+# For Lanisters!
+print('I am the King') # For Lannisters!
+
+# You know nothing, Jon Snow!
---
+++ code-basics.com/1. Osnovu_juzuka_Python/3. Instruczii (Statements).py
@@ -0,0 +1,10 @@
+print('Mother of Dragons.')
+print('Dracarys!')
+# => Mother of Dragons.
+# => Dracarys!
+print ('Выше мы говорили, что инструкции отделяются  друг от друга переходом на новую строку. Но есть и другой способ: их можно разделить точкой с запятой — ;:')
+print('Mother of Dragons.'); print('Drakarys!')
+
+print('Robert')
+print('Stannis')
+print('Renly')
