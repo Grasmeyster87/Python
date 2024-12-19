246375bc12b825714f70ee09900cc25733bdfaab
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-12-19 20:25:17 +0200

    17. Chto_takoe_peremennay

---
+++ code-basics.com/4. Peremennue.py/17. Chto_takoe_peremennay.py
@@ -0,0 +1,17 @@
+# greeting - переводится как приветствие
+
+greating = 'Father!'
+
+print(greating)
+print(greating)
+
+greeting1 = 'Father!'
+print(greeting1)
+print(greeting1)
+
+greeting2 = 'Mother!'
+print(greeting2)
+print(greeting2)
+
+motto = 'What Is Dead May Never Die!'
+print(motto)
