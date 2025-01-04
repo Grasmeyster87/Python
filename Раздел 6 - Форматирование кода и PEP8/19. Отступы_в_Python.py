76306ac19ad154d9eed51f02588080d854dc97d8
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-01-04 13:18:15 +0200

    Раздел 6 - Форматирование кода и PEP8

---
+++ Раздел 6 - Форматирование кода и PEP8/19. Отступы_в_Python.py
@@ -0,0 +1,5 @@
+def print_name_function(name):
+    print(name)
+
+
+print_name_function('Vitaliy')
---
+++ Раздел 6 - Форматирование кода и PEP8/21. Форматирование кода Python и PEP8.py
@@ -0,0 +1,10 @@
+my_list = [1, 2,   3]
+
+print(my_list)
+
+
+def my_fn():
+    print('Hello')
+
+
+print('Bogdan')
