684ff6fe919490de4d45ba154f4440a7fb489e31
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-01-19 14:58:17 +0200

    Раздел 25 - Колбэк функции

---
+++ Раздел 25 - Колбэк функции/103. Практика - Колбєки функции.py
@@ -0,0 +1,44 @@
+# ------------------ Пример 1 -----------------------
+
+def other_fn():
+    pass
+
+
+def fn_with_callback(callback_fn):
+    callback_fn()
+
+
+fn_with_callback(other_fn)
+
+
+def print_number_info(num):
+    if (num % 2) == 0:
+        print("Enter number is even")
+    else:
+        print("Entered number is odd")
+
+
+def print_square_num(num):
+    print("Square of the num is", num * num)
+
+
+def process_number(num, callback_fn):
+    callback_fn(num)
+
+
+entered_num = int(input('Enter any number:'))
+
+process_number(entered_num, print_number_info)
+process_number(entered_num, print_square_num)
+
+# ------------------ Пример 2 -----------------------
+
+def send_data(data):
+    return {'data': 'sending data to the remote server'}
+    
+
+def process_data(input_data, send_data_fn):
+    updated_data = input_data.copy()
+    send_data_fn(updated_data)
+
+process_data({'name': 'Vitaliy', send_data})
